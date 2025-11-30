24863111c9281dfffc55d8bc4e8ad9a60a0aee50
Russ Winchester <garcia.saylo3113@gmail.com>
2025-11-30 16:33:14 +0800

    first version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ChallengeGenerator
 Finals group
+ Team Activity: Challege Generator App
+
+ Objective:
+
+ Create Python app that can:
+    * Provides a CLI Challenger Generator to add players and scores
+    * Use OOP to manage objects
+    * Serves tha challenges via Flask so other teams can consume them (use with ngrok to espose publicly)
+    * Includs Git/Github usage notes and a full README.md for documentation
--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Vince Hee"
+# A list of fun physical or daily challenges
+CHALLENGES = [
+    "Do Aimlabs for 30 minutes",
+    "Play 10 deathmatches with vandal",
+    "Play 10 deathmatches with phantom",
+    "Play 10 deathmatches with sheriff",
+    "Play 10 deathmatches with ghost",
+    "Kill 25 hard bots in the range",
+    "Kill 100 bots in less than 100 seconds",
+    "Play 10 Teamdeathmatch",
+    "Play 5 competitive mode",
+    "Plant the spike 10 times",
+    "Defuse the spike 10 times",
+    "Kill 5 enemy with operator"
+]
 
-if __name__ == '__main__':
+@app.route("/")
+def index():
+    challenge = random.choice(CHALLENGES)
+    return render_template("index.html", challenge=challenge)
+
+if __name__ == "__main__":
     app.run(debug=True)
